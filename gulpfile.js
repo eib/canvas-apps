ebd29f588fd90379ffdefb38b6b07a3cb43ea3c7
Ethan Blackwelder <ethan.blackwelder@viterahealthcare.com>
2014-12-23 23:07:27 -0500

    Gulp smoosher.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    smoosher = require('gulp-smoosher');
 
 gulp.task('html', function () {
     gulp.src('src/**/*.html')
+        .pipe(smoosher())
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', ['html'], function () {
-    gulp.watch(['src/**/*.html'], ['html']);
+    gulp.watch(['src/**/*.html', 'src/**/bundle.js'], ['html']);
 });
 
 gulp.task('default', ['html']);
